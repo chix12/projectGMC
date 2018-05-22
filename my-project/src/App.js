987feef1cabba042836e2cd90e6d0ab13394fbd0
nadabk <nadbak95@gmail.com>
2018-05-22 13:44:27 +0100

    message

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import '/bootstrap/dist/css/bootstrap.css'
+
 class App extends Component {
       render () {
         return <h1>GENESIS TWO</h1>
