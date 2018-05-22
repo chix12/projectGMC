b4b228024ebe1eb8bed44ea1aed50fed9006df68
nadabk <nadbak95@gmail.com>
2018-05-22 15:27:46 +0100

    component login signup espaceetudiant

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css'
-import './App.css';
 
-import '/bootstrap/dist/css/bootstrap.css'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Login from './Components/Login/Login.js'
+import SignUp from './Components/SignUp/SignUp';
+import EspaceEtudiantMain from './Components/EspaceEtudiantMain/EspaceEtudiantMain';
+
 
 class App extends Component {
       render () {
-        return <h1>GENESIS TWO</h1>
+        return <EspaceEtudiantMain />
       }
 }
 
